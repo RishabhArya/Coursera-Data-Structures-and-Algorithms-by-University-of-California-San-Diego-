5ffa20c2ad20a56e13e18465be3c1a6d4a5125e7
Rishabh <rishabharya2799@gmail.com>
2020-06-06 07:44:23 +0530

    is bst

--- 2.Data Structures/week4_binary_search_trees/2_is_bst/is_bst.py
+++ 2.Data Structures/week4_binary_search_trees/2_is_bst/is_bst.py
@@ -1,25 +1,50 @@
-#!/usr/bin/python3
-
 import sys, threading
 
-sys.setrecursionlimit(10**7) # max depth of recursion
-threading.stack_size(2**25)  # new thread will get stack of such size
+sys.setrecursionlimit(10 ** 6)  # max depth of recursion
+threading.stack_size(2 ** 27)  # new thread will get stack of such size
 
-def IsBinarySearchTree(tree):
+
+class TreeOrders:
+    def read(self):
+        num = self.n = int(sys.stdin.readline())
+        if num == 0:
+          print("CORRECT")
+          exit()
+        self.key = [0 for i in range(self.n)]
+        self.left = [0 for i in range(self.n)]
+        self.right = [0 for i in range(self.n)]
+        for i in range(self.n):
+            [a, b, c] = map(int, sys.stdin.readline().split())
+            self.key[i] = a
+            self.left[i] = b
+            self.right[i] = c
+
+    def inOrder(self):
+        self.result = []
+        self.inOrderTraversal(0)
+        return self.result
+
+    def inOrderTraversal(self, index):
+        if self.left[index] != -1:
+            self.inOrderTraversal(self.left[index])
+        self.result.append(self.key[index])
+        if self.right[index] != -1:
+            self.inOrderTraversal(self.right[index])
+
+def checkBst(arr):
+  for x in range(len(arr) - 1):
+    if arr[x + 1] < arr[x]:
+      return False
 
   return True
 
-
-
 def main():
-  nodes = int(sys.stdin.readline().strip())
-  tree = []
-  for i in range(nodes):
-    tree.append(list(map(int, sys.stdin.readline().strip().split())))
-  print(tree)
-  if IsBinarySearchTree(tree):
-    print("CORRECT")
-  else:
-    print("INCORRECT")
+    tree = TreeOrders()
+    tree.read()
+    arr = tree.inOrder()
+    if checkBst(arr):
+      print("CORRECT")
+    else:
+      print("INCORRECT")
 
 threading.Thread(target=main).start()
